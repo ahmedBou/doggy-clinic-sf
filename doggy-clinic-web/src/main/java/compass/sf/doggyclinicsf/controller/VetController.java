4c734364eb71cc0b0f67e9627c816ad29ff76a5f
ahmed boutayeb <instinctauto@gmail.com>
2021-05-20 21:04:18 +0100

    Creer index page veterinaire et le Controllers, Closes #0.

---
+++ doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/VetController.java
@@ -0,0 +1,13 @@
+package compass.sf.doggyclinicsf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","vets/index", "vets/index.html" })
+    public String listVet(){
+        return "vets/index";
+    }
+}
