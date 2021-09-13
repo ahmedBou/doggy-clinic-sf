38f800dd4ab7e8eeec24bd11f3e0c94cdaef8c86
ahmed <ahmedBoutayeb@my.uopeople.edu>
2021-09-13 22:04:58 +0100

    added vets list and update the controller . Closes #13

--- doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/VetController.java
+++ doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/VetController.java
@@ -1,14 +1,23 @@
 package compass.sf.doggyclinicsf.controller;
 
+import compass.sf.doggyclinicsf.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","vets/index", "vets/index.html" })
-    public String listVet(){
+    public String listVet(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
