ae4ee7532f0a862fe280e4736faf5dbecaa26971
ahmed <ahmedBoutayeb@my.uopeople.edu>
2021-09-13 21:46:08 +0100

    added owners list and update the controller . Closes #14

--- doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/OwnerController.java
+++ doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/OwnerController.java
@@ -1,15 +1,25 @@
 package compass.sf.doggyclinicsf.controller;
 
 
+import compass.sf.doggyclinicsf.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owner"})
 @Controller
 public class OwnerController {
 
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String ownerList(){
+    public String ownerList(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
