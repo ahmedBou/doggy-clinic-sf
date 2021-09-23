e256fbe7af7931a112b70e782089a076e4b356a2
ahmed <ahmedBoutayeb@my.uopeople.edu>
2021-09-23 14:49:30 +0100

    create jpa for owner and her relation. Closes #27

--- doggy-clinic-data/src/main/java/compass/sf/doggyclinicsf/model/Doggy.java
+++ doggy-clinic-data/src/main/java/compass/sf/doggyclinicsf/model/Doggy.java
@@ -1,11 +1,23 @@
 package compass.sf.doggyclinicsf.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name= "doggy")
 public class Doggy extends BaseEntity{
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private DoggyType doggyType;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
--- doggy-clinic-data/src/main/java/compass/sf/doggyclinicsf/model/DoggyType.java
+++ doggy-clinic-data/src/main/java/compass/sf/doggyclinicsf/model/DoggyType.java
@@ -1,6 +1,13 @@
 package compass.sf.doggyclinicsf.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="doggy_type")
 public class DoggyType extends BaseEntity{
+    @Column(name = "names")
     private String names;
 
     public String getName() {
