8ffcfcc4853f9ecfd8790a5de9e4ea42e0c5736b
ahmed <ahmedBoutayeb@my.uopeople.edu>
2021-10-07 13:13:35 +0100

    Add Json endpoint to Vet .close #57

--- doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/VetController.java
+++ doggy-clinic-web/src/main/java/compass/sf/doggyclinicsf/controller/VetController.java
@@ -1,11 +1,14 @@
 package compass.sf.doggyclinicsf.controller;
 
+import compass.sf.doggyclinicsf.model.Vet;
 import compass.sf.doggyclinicsf.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
+import java.util.Set;
 @Controller
 public class VetController {
 
@@ -19,5 +22,10 @@ public class VetController {
     public String listVet(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
